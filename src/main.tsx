bae1fb2d156a718c47d0978b8d828689f2b84f34
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-08-14 14:45:37 +0530

    deploy err

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "rsuite/dist/rsuite.min.css";
+import './index.css'
 import { BrowserRouter  } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
